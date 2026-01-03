bb5939d74ad753b09a84783d3c5048608a69352c
harsh <harshwardhany87@gmail.com>
2026-01-03 15:37:26 +0530

    code

--- backend/src/Routes/userRoutes.ts
+++ backend/src/Routes/userRoutes.ts
@@ -1,11 +1,11 @@
 import {Router} from 'express';
 const userRouter=Router();
 import { getSignUp,getAll,getSignIn,forgotPassword } from '../Controllers/userControllers';
-import verifyToken from '../middleware/verifyToken';
+
 
 userRouter.get("/allUser",getAll);
 userRouter.post("/getSignUp",getSignUp);
 userRouter.post("/getSignIn",getSignIn);
-userRouter.post("/forgotPassword",verifyToken,forgotPassword);
+userRouter.post("/forgotPassword",forgotPassword);
 
 export default userRouter;
